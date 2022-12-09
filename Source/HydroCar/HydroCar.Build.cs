6f25acac028f84ae6c57d29261517dc87a5c6a11
Calvin-Ruiz <calvin.ruiz@epitech.eu>
2022-12-09 10:06:17 +0100

    Fix compilation on windows

--- Source/HydroCar/HydroCar.Build.cs
+++ Source/HydroCar/HydroCar.Build.cs
@@ -8,15 +8,15 @@ public class HydroCar : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Cpp20;
-		bWarningsAsErrors = false; 
-	
+		// bWarningsAsErrors = false;
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
